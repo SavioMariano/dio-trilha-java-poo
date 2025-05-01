594540994c0210fa2e8ae0d925f8cf73926bc2c1
Savio <78896606+SavioMariano@users.noreply.github.com>
2025-05-01 14:09:25 -0300

    Delete dio-trilha-java-poo directory

--- dio-trilha-java-poo/interfaces/src/equipamentos/digitalizadora/Scanner.java
+++
@@ -1,8 +0,0 @@
-package equipamentos.digitalizadora;
-public class Scanner implements Digitalizadora {
-    
-    public void digitalizar() {
-        System.out.println("DIGITALIZAR");
-    }
-
-}
--- dio-trilha-java-poo/interfaces/src/equipamentos/impressora/Deskjet.java
+++
@@ -1,8 +0,0 @@
-package equipamentos.impressora;
-public class Deskjet implements Impressora{
-
-    public void imprimir() {
-        System.out.println("IMPRIMINDO");
-    }
-
-}
